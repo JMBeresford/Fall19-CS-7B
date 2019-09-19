90b60399783a8c8371247b38952dbe8b595585b9
John Beresford <jberesford@volcaus.com>
2019-09-18 20:13:37 -0700

    init

---
+++ Card.h
@@ -0,0 +1,19 @@
+#ifndef CARD_H
+#define CARD_H
+#include <string>
+
+
+class Card
+{
+public:
+    Card();
+    Card(std::string, std::string);
+
+    std::string getSuit();
+    std::string getRank();
+    std::string getCard();
+
+private:
+    std::string suit, rank;
+};
+#endif
---
+++ Deck.cpp
@@ -0,0 +1,61 @@
+#include "Deck.h"
+
+
+Deck::Deck()
+{
+    this->_deck.clear();
+
+    for (auto suit : this->suits)
+        for (auto rank : this-> ranks)
+            this->_deck.push_back(Card(suit, rank));
+}
+
+Deck::Deck(size_t i)
+{
+    for (auto suit : this-> suits)
+    {
+        if (this->_deck.size() >= i)
+            break;
+        else
+            for (auto rank : this->ranks)
+            {
+                if (this->_deck.size() >= i)
+                    break;
+                else
+                    this->_deck.push_back(Card(suit, rank));
+            }
+    }
+}
+
+std::string Deck::display()
+{
+    std::string str = "";
+
+    for (auto card : this->_deck)
+        str += card.getCard() + "\n";
+
+    return str;
+}
+
+std::vector<Card> Deck::getDeck()
+{
+    return this->_deck;
+}
+
+Deck Deck::operator+(const Deck& rhs)
+{
+    Deck rTemp = rhs;
+    Deck lTemp;
+    
+    lTemp._deck = this->_deck;
+    
+    for (auto card : rTemp.getDeck())
+        lTemp._deck.push_back(card);
+
+    return lTemp;
+}
+
+Card& Deck::operator[](int i)
+{
+    return this->_deck[i];
+}
